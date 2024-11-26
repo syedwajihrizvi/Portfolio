67fd43d89d10affac49dc04aa365474a3b2389d6
AB Rizvi <abrizvi@ABs-MacBook-Pro.local>
2024-11-25 23:36:01 -0500

    Added spotlight effect

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,6 +6,7 @@ import CssBaseline from '@mui/material/CssBaseline'
 import "../App.css"
 import "../assets/css/main.css"
 import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
 
 const darkTheme = createTheme({
     palette: {
@@ -14,8 +15,27 @@ const darkTheme = createTheme({
 });
 
 function Main() {
+
+    useEffect(() => {
+        const mouseSpotlight = document.documentElement.querySelector('.mouse-spotlight') as HTMLElement
+        document.addEventListener('mousemove', (event) => {
+            if (mouseSpotlight)
+                mouseSpotlight.style.left = `${event.clientX}px`
+                mouseSpotlight.style.top = `${event.clientY}px`
+                mouseSpotlight.style.opacity = '1'
+        })
+
+        document.addEventListener('mouseleave', () => {
+            if (mouseSpotlight)
+                mouseSpotlight.style.left = '0'
+                mouseSpotlight.style.top = '0'
+                mouseSpotlight.style.opacity = '0'
+        })
+    })
+
     return (
         <ThemeProvider theme={darkTheme}>
+            <div className='mouse-spotlight'></div>
             <CssBaseline>
                 <Box className="main-grid">
                     <Box className="sidebar">
